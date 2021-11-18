a2442f35f195105dd62d0bbc92c42224eccbc1c7
gururepala <guru.repala@gmail.com>
2021-11-18 22:31:20 +0530

    added commands

--- Jenkins_Build.groovy
+++ Jenkins_Build.groovy
@@ -11,6 +11,8 @@ pipeline{
         stage("Build Code") {
             steps {
                println "mvn clean package"
+               sh "mvn clean package"
+               sh "ls -l /target/"
             }
         }
         stage("Uploading build files to S3 bucket") {
