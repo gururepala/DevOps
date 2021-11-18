ade991a13422269e92f89842f537d35cc18ec423
gururepala <guru.repala@gmail.com>
2021-11-18 23:34:46 +0530

    Added variables

--- Jenkins_Build.groovy
+++ Jenkins_Build.groovy
@@ -1,11 +1,13 @@
 //Declarative pipeline
 pipeline{
     agent any
+    environment {
+        BRANCH = "${env.BRANCH_NAME}"
     stages{
         stage("Cloning code") {
             steps {
                println "Git clone URL"
-               checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-user-github', url: 'https://github.com/gururepala/boxfuse-sample-java-war-hello.git']]])
+               checkout([$class: 'GitSCM', branches: [[name: '*/${BRANCH}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-user-github', url: 'https://github.com/gururepala/boxfuse-sample-java-war-hello.git']]])
                sh "ls -l"
             }
         }
@@ -20,7 +22,7 @@ pipeline{
             steps {
                 println "upload files to S3"
                 sh "echo $BUILD_NUMBER"
-                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mydeployedprojects/Dfiles/{$BUILD_NUMBER}/"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mydeployedprojects/${BRANCH}/${BUILD_NUMBER}/"
             }
         }
     }
